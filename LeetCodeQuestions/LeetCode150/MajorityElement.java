4f14da4cea950940b2cd220fb802adcc1d1d2451
Saksham Sharma <sakshamshankyan7@gmail.com>
2024-08-14 21:17:32 +0530

    6/150

---
+++ LeetCodeQuestions/LeetCode150/MajorityElement.java
@@ -0,0 +1,23 @@
+package LeetCodeQuestions.LeetCode150;
+
+public class MajorityElement {
+
+    public static void findMajority(int[] nums){
+        int ele = nums[0];
+        int count = 1;
+        for(int i=0;i<nums.length;i++){
+            if(ele == nums[i]){
+                count++;
+            }else if(count == 0){
+                ele = nums[i];
+            }else{
+                count--;
+            }
+        }
+        System.out.println(ele);
+    }
+    public static void main(String[] args) {
+        int[] nums = {3,2,3};
+        findMajority(nums);
+    }
+}
