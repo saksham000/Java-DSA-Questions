1bbeb1679734b1ca1aa25145a1395fa0a2473fa9
saksham000 <sakshamshankyan7@gmail.com>
2024-12-11 23:53:01 +0530

    linkedlist done

--- Striver/Arrays/OldCode/class1.java
+++
@@ -1,9 +0,0 @@
-//This is how methods are created
-public class class1{
-    public void carname(String name){
-        System.out.print("The top Speed of car " + name);
-    }
-    public void topspeed(int speed){
-        System.out.println(" is " + speed);
-    }
-}
--- Striver/LinkedList/AddTwoNumRepAsLinkedList.java
+++ Striver/LinkedList/AddTwoNumRepAsLinkedList.java
@@ -19,29 +19,25 @@ public class AddTwoNumRepAsLinkedList {
 
     public static void addTwoNumRepAsLinkedList(Node list1, Node list2) {
         Node dummyNode = new Node(-1);
-        Node tempNode = dummyNode;
+        Node temp = dummyNode;
         int carry = 0;
-
-        while (list1 != null || list2 != null || carry == 1) {
+        while(list1 != null || list2 != null || carry == 1){
             int sum = 0;
-
-            if (list1 != null) {
+            if(list1 != null){
                 sum += list1.data;
                 list1 = list1.next;
             }
-            if (list2 != null) {
+            if(list2 != null){
                 sum += list2.data;
                 list2 = list2.next;
             }
-
-            sum += carry;
-            carry = sum / 10;
-            Node newNode = new Node(sum % 10);
-            tempNode.next = newNode;
-            tempNode = tempNode.next;
+            sum = sum + carry;
+            carry = sum/10;
+            Node newNode = new Node(sum%10);
+            temp.next = newNode;
+            temp = temp.next;
         }
         printList(dummyNode.next);
-
     }
 
     public static void printList(Node head) {
