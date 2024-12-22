ada76dcf3e5b0cb66553faea778d50127ea1cd81
saksham000 <sakshamshankyan7@gmail.com>
2024-12-22 21:36:33 +0530

    done some leet150

---
+++ LeetCode150/GasStation.java
@@ -0,0 +1,33 @@
+package LeetCode150;
+
+public class GasStation {
+    public static void findStarting(int[] gas, int[] cost) {
+        int totalGas = 0;
+        int totalCost = 0;
+
+        for (int i = 0; i < gas.length; i++) {
+            totalGas += gas[i];
+            totalCost += cost[i];
+        }
+        if (totalCost > totalGas) {
+            System.out.println("-1");
+        }
+        int currentGas = 0;
+        int start = 0;
+
+        for (int i = 0; i < gas.length; i++) {
+            currentGas += gas[i] - cost[i];
+            if (currentGas < 0) {
+                currentGas = 0;
+                start = i + 1;
+            }
+        }
+        System.out.println(start);
+    }
+
+    public static void main(String[] args) {
+        int[] gas = { 1, 2, 3, 4, 5 };
+        int[] cost = { 3, 4, 5, 1, 2 };
+        findStarting(gas, cost);
+    }
+}
--- LeetCode150/revision/rev.java
+++
@@ -1,7 +0,0 @@
-package LeetCode150.revision;
-
-public class rev {
-    System.out.println("hello");
-    //will do some code in 1 day
-    
-}
