8d8c6c5d882a7f7a4c9386440d4605f020454a03
saksham000 <sakshamshankyan7@gmail.com>
2025-01-11 00:42:27 +0530

    one more leet150

--- LeetCode150/MinStack.java
+++ LeetCode150/MinStack.java
@@ -1,5 +1,35 @@
 package LeetCode150;
 
+import java.util.Stack;
+
 public class MinStack {
-    
+    private Stack<Integer> mainStack;
+    private Stack<Integer> minStack;
+
+    public MinStack() {
+        mainStack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        mainStack.push(val);
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
+        }
+    }
+
+    public void pop() {
+        if (minStack.peek().equals(mainStack.peek())) {
+            minStack.pop();
+        }
+        mainStack.pop();
+    }
+
+    public int top() {
+        return mainStack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
 }
