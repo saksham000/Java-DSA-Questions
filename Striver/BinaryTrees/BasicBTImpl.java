2be5f466230903e629cf4b0b40cf7414cf05637a
saksham000 <sakshamshankyan7@gmail.com>
2025-01-29 22:54:55 +0530

    done some BT

--- Striver/BinaryTrees/BasicBTImpl.java
+++ Striver/BinaryTrees/BasicBTImpl.java
@@ -14,11 +14,11 @@ public class BasicBTImpl {
     public static Node basicBT() {
         Node root = new Node(1);
         root.left = new Node(2);
-        root.right = new Node(3);
-        root.left.left = new Node(4);
-        root.left.right = new Node(5);
-        root.right.left = new Node(6);
-        root.right.right = new Node(7);
+        root.right = new Node(2);
+        root.left.left = new Node(3);
+        root.left.right = new Node(4);
+        root.right.left = new Node(4);
+        root.right.right = new Node(3);
         return root;
     }
 }
--- Striver/BinaryTrees/VerticalOrderTraversalBinaryTree.java
+++
@@ -1,5 +0,0 @@
-package Striver.BinaryTrees;
-
-public class VerticalOrderTraversalBinaryTree {
-    // https://leetcode.com/problems/vertical-order-traversal-of-a-binary-tree/description/
-}
